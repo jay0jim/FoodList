d587365cc245cc2c0aea342a92c247e416578187
Tony <284096905@qq.com>
2015-08-02 09:59:10 +0800

    Initial Commit

---
+++ FoodList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FoodList
+//
+//  Created by Tony on 15/8/2.
+//  Copyright (c) 2015年 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
